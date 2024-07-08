fabde339ab197e5a5999bceadbdadc935e47ec7f
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-08 19:06:57 +0200

    Initial Commit

---
+++ Volume Converter/Volume_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volume_ConverterApp.swift
+//  Volume Converter
+//
+//  Created by Adam Elfsborg on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct Volume_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
